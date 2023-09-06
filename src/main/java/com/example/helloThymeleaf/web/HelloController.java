16a146e3af0a36cea5a88f33a14d6ce57c67704a
Costin86 <constantin.sarb@yahoo.com>
2023-09-06 13:30:02 +0300

    first commit

---
+++ src/main/java/com/example/helloThymeleaf/web/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.helloThymeleaf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name, @RequestParam int age, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "greeting";
+    }
+}
